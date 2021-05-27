01bf5d1b1e060c556d8db620214918d54c50d945
Nikhil Gupta <nikhil73533@gmail.com>
2021-05-27 20:03:09 +0530

    Deploy project

--- Restaurant_Management_System/Restaurant_Management_System/settings.py
+++ Restaurant_Management_System/Restaurant_Management_System/settings.py
@@ -123,6 +123,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'Restaurant_Management_System/static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
